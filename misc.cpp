a2094ef11a0e8a54fd0900982f9ff5639c452c2f
Crappyraj <67316208+Crappyraj@users.noreply.github.com>
2020-11-18 08:47:39 +0530

    Add files via upload

---
+++ misc.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"this is one sentence \n";
+    cout<<"this is another \n";
+    cout<<"this is one sentence"<<endl;
+    cout<<"this is another"<<endl;
+
+}
