2d9724195ca97d7bd70ee8b391850148804ebee4
Crappyraj <67316208+Crappyraj@users.noreply.github.com>
2020-11-24 06:52:37 +0530

    Add files via upload

---
+++ max.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    if(a>b)
+    {
+        cout<<"maximum="<< a<<endl;
+        cout<<"minimum="<< b<<endl;
+    }
+    else
+    {
+       cout<<"maximum="<< b<<endl;
+      cout<<"minimum="<< a<<endl; 
+    }
+    
+}
