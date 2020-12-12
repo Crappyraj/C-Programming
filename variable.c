cd273d2d9bf345e0ea2491ae7a10bbb8da68abfa
Crappyraj <67316208+Crappyraj@users.noreply.github.com>
2020-12-12 18:37:10 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int inum=10;
+    float fnum=10.2;
+    double dnum=10.2;
+    long lnum=10;
+    char cname='A';
+    char sname[10]="KIET";
+    printf("int =%d, float =%f, double =%lf, long =%ld, char =%c, String =%s",inum,fnum,dnum,lnum,cname,sname);
+    return 0;
+}
