0071a819f166f14e7dc5880046a2666545c6211a
Crappyraj <67316208+Crappyraj@users.noreply.github.com>
2020-12-13 20:15:55 +0530

    Add files via upload

---
+++ funlargest.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int largest(int a,int b,int c)
+{
+	if((a>b)&&(a>c))
+	{
+		return a;
+	}
+	if((b>a)&&(b>c))
+	{
+		return b;
+	}
+	if((c>a)&&(c>b))
+	{
+		return c;
+	}
+}
+main()
+{
+	int n=largest(4,5,6);
+	printf("%d",n);
+}
+
+
